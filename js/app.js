e9d85ab6751f40fa1ec3f538f44be820436464d3
critter1996 <109566820+critter1996@users.noreply.github.com>
2024-10-09 20:24:00 -0400

    Added Slider section / cleaned up code

--- js/app.js
+++ js/app.js
@@ -1,24 +1,71 @@
+///////////////////////////To-Do List/////////////////////////////////
+// 1. Ability to push new project
+// 2. Search Function
+// 3. Gallery widget carusel
+
+//4. Create Grid Design?
+//5. Circle photo?
+//<!-- Add Scroll to top to both pages -->
+
+
+
+
 //Wraps the page to force it to load last
     document.addEventListener('DOMContentLoaded', () => {
 
 
 
+// IMAGE SLIDER
+
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalId = null;
 
 
+initializeSlider();
+function initializeSlider() {
+
+  if(slides.length > 0){
+    slides[slideIndex].classList.add('displaySlide');
+    intervalId = setInterval(nextSlide, 5000);
+  }
+
+}
 
 
+function showSlide(index) {
+
+  if(index >= slides.length){
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach(slide => {
+    slide.classList.remove('displaySlide');
+  });
+  slides[slideIndex].classList.add('displaySlide');
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex)
+}
 
 
+// //Next Slide Button
+// addEventListener() {
 
+// }
 
+// //Prev Slide Button
+// addEventListener() {
 
-
-
-
-
-
-
-
-
+// }
         
     })
